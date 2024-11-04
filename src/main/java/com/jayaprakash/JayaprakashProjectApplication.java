152e542fb8f6cfd958ce76c8b48c5943c04fe26b
bhuvaneswar <bhuvaneswarr412@gmail.com.com>
2024-11-04 10:01:16 +0530

    first

---
+++ src/main/java/com/jayaprakash/JayaprakashProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jayaprakash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JayaprakashProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JayaprakashProjectApplication.class, args);
+	}
+
+}
